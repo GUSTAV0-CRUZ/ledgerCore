6674402921458c7075e6294ee678c92817028c11
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-06-12 22:59:52 -0300

    adiciona docker compose com a infra: postgres e rabbitMQ, e adiciona o microservice: service-bancary

---
+++ service-bancary/src/main/java/dev/cruzs/gustavo/service_bancary/ServiceBancaryApplication.java
@@ -0,0 +1,13 @@
+package dev.cruzs.gustavo.service_bancary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBancaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBancaryApplication.class, args);
+	}
+
+}
---
+++ service-bancary/src/test/java/dev/cruzs/gustavo/service_bancary/ServiceBancaryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cruzs.gustavo.service_bancary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBancaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
