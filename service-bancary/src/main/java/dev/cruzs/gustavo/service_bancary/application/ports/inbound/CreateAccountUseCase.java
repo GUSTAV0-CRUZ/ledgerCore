1366a1a8941293df6bccc3edd8156d7cf56691a8
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-06-13 21:09:52 -0300

    renomea nome das class

--- service-bancary/src/main/java/dev/cruzs/gustavo/service_bancary/application/ports/inbound/CreateAccountUseCase.java
+++ service-bancary/src/main/java/dev/cruzs/gustavo/service_bancary/application/ports/inbound/CreateAccountUseCase.java
@@ -1,8 +1,8 @@
 package dev.cruzs.gustavo.service_bancary.application.ports.inbound;
 
-import dev.cruzs.gustavo.service_bancary.application.ports.inbound.dtos.CreateAccountDto;
+import dev.cruzs.gustavo.service_bancary.application.ports.inbound.commands.CreateAccountCommand;
 import dev.cruzs.gustavo.service_bancary.domain.Account;
 
 public interface CreateAccountUseCase {
-  Account execute(CreateAccountDto createAccountDto);
+  Account execute(CreateAccountCommand createAccountCommand);
 }
--- service-bancary/src/main/java/dev/cruzs/gustavo/service_bancary/application/ports/inbound/commands/CreateAccountCommand.java
+++ service-bancary/src/main/java/dev/cruzs/gustavo/service_bancary/application/ports/inbound/commands/CreateAccountCommand.java
@@ -1,11 +1,11 @@
-package dev.cruzs.gustavo.service_bancary.application.ports.inbound.dtos;
+package dev.cruzs.gustavo.service_bancary.application.ports.inbound.commands;
 
 import dev.cruzs.gustavo.service_bancary.domain.enums.AccountTypeEnum;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
-public record CreateAccountDto(
+public record CreateAccountCommand(
     UUID userId,
     Integer agency,
     String number,
