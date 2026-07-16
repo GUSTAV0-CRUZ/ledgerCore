1068149b7f6623f068b96f89bdd963c8755dcb65
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-07-16 15:42:26 -0300

    feat(auth-service): configura o application.yaml para usar o rabbitmq

--- auth-service/src/main/java/dev/cruzs/gustavo/auth_service/adapters/configs/ConsumersConfig.java
+++ auth-service/src/main/java/dev/cruzs/gustavo/auth_service/adapters/configs/ConsumersConfig.java
@@ -2,12 +2,14 @@ package dev.cruzs.gustavo.auth_service.adapters.configs;
 
 import dev.cruzs.gustavo.auth_service.adapters.in.dtos.CreateUserRequestDto;
 import dev.cruzs.gustavo.auth_service.adapters.in.messaging.UserConsumer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.function.Consumer;
 
 @Configuration
 public class ConsumersConfig {
+  @Bean
   public Consumer<CreateUserRequestDto> createUserConsumer(UserConsumer userConsumer) {
     return userConsumer::createUserConsumer;
   }
