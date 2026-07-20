80bdafff7c861689876574e6d3bbb96faed85813
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-07-20 13:04:19 -0300

    feat(gateway-service): adiciona validação do dto

--- gateway-service/src/main/java/dev/cruzs/gustavo/gateway_service/filters/messages/CreateUserProducerGatewayFilterFactory.java
+++ gateway-service/src/main/java/dev/cruzs/gustavo/gateway_service/filters/messages/CreateUserProducerGatewayFilterFactory.java
@@ -1,6 +1,8 @@
 package dev.cruzs.gustavo.gateway_service.filters.messages;
 
 import dev.cruzs.gustavo.gateway_service.filters.messages.dtos.CreateUserDto;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
@@ -11,19 +13,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Component
 public class CreateUserProducerGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
   private final Logger logger = LoggerFactory.getLogger(CreateUserProducerGatewayFilterFactory.class);
   private final StreamBridge streamBridge;
   private final ModifyRequestBodyGatewayFilterFactory modifyRequestBodyGatewayFilterFactory;
+  private final Validator validator;
 
   public CreateUserProducerGatewayFilterFactory(
       StreamBridge streamBridge,
-      ModifyRequestBodyGatewayFilterFactory modifyRequestBodyGatewayFilterFactory
+      ModifyRequestBodyGatewayFilterFactory modifyRequestBodyGatewayFilterFactory, Validator validator
   ) {
     super(Object.class);
     this.streamBridge = streamBridge;
     this.modifyRequestBodyGatewayFilterFactory = modifyRequestBodyGatewayFilterFactory;
+    this.validator = validator;
   }
 
   @Override
@@ -36,6 +43,16 @@ public class CreateUserProducerGatewayFilterFactory extends AbstractGatewayFilte
         String.class,
         (exchange, createUserDto) -> {
           if (createUserDto == null) throw new IllegalArgumentException("createUserDto is null");
+
+          Set<ConstraintViolation<CreateUserDto>> violations = this.validator.validate(createUserDto);
+
+          if (!violations.isEmpty()) {
+            String messagesError = violations.stream()
+                .map(v -> v.getPropertyPath() + ": " + v.getMessage())
+                .collect(Collectors.joining(", "));
+
+            throw new IllegalArgumentException(messagesError);
+          }
 
           this.streamBridge.send("createUserProducer-out-0", createUserDto);
           this.logger.info("Try create user, with email: ({})", createUserDto.email());
--- gateway-service/src/main/java/dev/cruzs/gustavo/gateway_service/filters/messages/dtos/CreateUserDto.java
+++ gateway-service/src/main/java/dev/cruzs/gustavo/gateway_service/filters/messages/dtos/CreateUserDto.java
@@ -1,4 +1,20 @@
 package dev.cruzs.gustavo.gateway_service.filters.messages.dtos;
 
-public record CreateUserDto(String username, String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateUserDto(
+    @NotBlank
+    @Size(min = 3,  max = 32)
+    String username,
+
+    @Email
+    @NotBlank
+    String email,
+
+    @NotBlank
+    @Size(min = 8)
+    String password
+) {
 }
