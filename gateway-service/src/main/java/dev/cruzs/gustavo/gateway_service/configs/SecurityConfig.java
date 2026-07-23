942368113aa5e54ee85e6bb98a39b2d27d9a563a
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-07-22 21:04:48 -0300

    feat(gateway-service/configs): libera rotas de auth e decodifica o jwt

---
+++ gateway-service/src/main/java/dev/cruzs/gustavo/gateway_service/configs/SecurityConfig.java
@@ -0,0 +1,45 @@
+package dev.cruzs.gustavo.gateway_service.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+  @Value("${jwt.secrete.key}")
+  private String secreteKeyJwt;
+
+  @Bean
+  public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    return http
+        .csrf(csrf -> csrf.disable())
+        .authorizeExchange(exchanges -> exchanges
+            .pathMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll()
+            .anyExchange().authenticated()
+        )
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(
+            jwt -> jwt.jwtDecoder(this.jwtDecoder())
+            )
+        )
+        .build();
+  }
+
+  public ReactiveJwtDecoder jwtDecoder() {
+    SecretKey secretKey = new SecretKeySpec(
+        this.secreteKeyJwt.getBytes(StandardCharsets.UTF_8),
+        "HmacSHA256"
+    );
+    return NimbusReactiveJwtDecoder.withSecretKey(secretKey).build();
+  }
+}
